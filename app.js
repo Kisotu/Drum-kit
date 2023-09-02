569a5b992f2ef3035cd0b6daa60cc53889e7d639
Oloishorua <jacoboloishorua@gmail.com>
2023-09-02 22:20:10 +0300

    code refactor

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 
 const playAudio = (e) => {
 const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if (!audio) return; // stop if null
   audio.currentTime = 0; //rewinds to start
   audio.play();
